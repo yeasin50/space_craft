4a05d24fbaac8f75eaa8fcab07a9347dfdf2a58d
MD.Yeasin Sheikh <46500228+yeasin50@users.noreply.github.com>
2021-05-29 20:44:58 +0600

    Vector2Position

---
+++ lib/SpaceCraft/model/vector2.dart
@@ -0,0 +1,12 @@
+/// Helper class TO hold position in 2D,
+///`dX = Left to Right` ,,
+///`dY = Top to Bottom`
+class Position2D {
+  double dX;
+  double dY;
+
+  Position2D({
+    this.dX = 0,
+    this.dY = 0,
+  });
+}

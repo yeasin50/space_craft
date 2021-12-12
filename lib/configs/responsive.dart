04f705444fe89c798131a0be99b8031275ce61e2
yeasin50 <yeasinsheikh50@gmail.com>
2021-12-12 22:19:33 +0600

    simplified-responsive-ui

---
+++ lib/configs/responsive.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+const double kMaxWidth = 1200.0;
+const double kMobileMaxWidth = 650.0;
+const double kTabletMaxWidth = 1130.0;
+
+class Responsive extends StatelessWidget {
+  final Widget mobile;
+  final Widget tablet;
+  final Widget desktop;
+
+  const Responsive({
+    required this.mobile,
+    required this.tablet,
+    required this.desktop,
+  });
+
+// This size work fine on my design, maybe you need some customization depends on your design
+
+  // This isMobile, isTablet, isDesktop helep us later
+  static bool isMobile(BuildContext context) =>
+      MediaQuery.of(context).size.width < kMobileMaxWidth;
+
+  static bool isTablet(BuildContext context) =>
+      MediaQuery.of(context).size.width < kTabletMaxWidth &&
+      MediaQuery.of(context).size.width >= kMobileMaxWidth;
+
+  static bool isDesktop(BuildContext context) =>
+      MediaQuery.of(context).size.width >= kTabletMaxWidth;
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      // If our width is more than 1100 then we consider it a desktop
+      builder: (context, constraints) {
+        if (constraints.maxWidth >= kTabletMaxWidth) {
+          return desktop;
+        }
+        // If width it less then 1100 and more then 650 we consider it as tablet
+        else if (constraints.maxWidth >= kMobileMaxWidth) {
+          return tablet;
+        }
+        // Or less then that we called it mobile
+        else {
+          return mobile;
+        }
+      },
+    );
+  }
+}
--- lib/widget/health_meter.dart
+++ lib/widget/health_meter.dart
@@ -1,19 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
- 
+
 import '../GameManager/playerManager.dart';
-import '../configs/size.dart';
 
 class HealthMeter extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    SizeConfig().init(context);
     return Consumer(
       builder: (ctx, data, ch) => Container(
-         margin: EdgeInsets.symmetric( vertical: 25),
-        
+        margin: EdgeInsets.symmetric(vertical: 25),
         child: LinearProgressIndicator(
-          value:  .01,
+          value: .01,
           backgroundColor: Colors.yellow,
           valueColor: AlwaysStoppedAnimation<Color>(Colors.red),
         ),

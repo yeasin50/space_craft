dd99afb7d4d5ee40ae735cd0450f4f0700b75a57
yeasin50 <yeasinsheikh50@gmail.com>
2022-01-03 23:42:11 +0600

    test-heart

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:space_craft/utils/utils.dart';
+
 import 'package:space_craft/widget/magic_ball.dart';
 
 import 'screens/on_play/on_play.screen.dart';
@@ -10,31 +12,103 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:
-          // _TestCases(),
-          OnPlayScreen(),
+      home: _TestCases(),
+      // OnPlayScreen(),
     );
   }
 }
 
-class _TestCases extends StatelessWidget {
+class _TestCases extends StatefulWidget {
   const _TestCases({Key? key}) : super(key: key);
 
+  @override
+  State<_TestCases> createState() => _TestCasesState();
+}
+
+class _TestCasesState extends State<_TestCases> {
+  double sliderVal = .2;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Container(
-        color: Colors.cyanAccent.withOpacity(.2),
-        child: Stack(
-          children: [
-            ParticleWidget(
-              debugMode: true,
-              callback: (id) {},
-              id: 2,
-              parentSize: const Size(100, 100),
-            ),
-          ],
+      body: Column(
+        children: [
+          Slider(
+            value: sliderVal,
+            onChanged: (value) {
+              setState(() {
+                sliderVal = value;
+              });
+            },
+          ),
+          LiveBar(
+            liveValue: sliderVal * 100,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class LiveBar extends StatefulWidget {
+  const LiveBar({
+    Key? key,
+    required this.liveValue,
+  }) : super(key: key);
+
+  final double liveValue;
+
+  @override
+  State<LiveBar> createState() => _LiveBarState();
+}
+
+class _LiveBarState extends State<LiveBar> with SingleTickerProviderStateMixin {
+  late Animation<Color> colorAnimation;
+
+  late AnimationController controller;
+
+  @override
+  void initState() {
+    controller = AnimationController(vsync: this);
+
+    TweenSequence(
+      [
+        TweenSequenceItem(
+          tween: ColorTween(
+            begin: Colors.redAccent,
+            end: Colors.red.shade300,
+          ),
+          weight: 1,
+        ),
+      ],
+    ).animate(controller);
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: const BorderRadius.all(Radius.circular(40)),
+      child: SizedBox(
+        width: 400,
+        height: 100,
+        child: ColoredBox(
+          color: Colors.grey,
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Text(widget.liveValue.toStringAsFixed(0)),
+              Align(
+                alignment: const Alignment(-.95, 0),
+                child: Icon(
+                  Icons.favorite,
+                  color: changeColorHue(
+                      color: Colors.red, increaseBy: widget.liveValue),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );

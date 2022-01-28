6372648dc8e35e5d1db92a065bbf4351b3d47eac
yeasin50 <yeasinsheikh50@gmail.com>
2022-01-28 20:24:58 +0600

    added-play-screen

--- lib/app.dart
+++ lib/app.dart
@@ -22,8 +22,8 @@ class App extends StatelessWidget {
         scaffoldBackgroundColor: Colors.black,
       ),
       // home: const _TestCases(),
-      // home: OnPlayScreen(),
-      home: const StartScreen(),
+      home: const OnPlayScreen(),
+      // home: const StartScreen(),
     );
   }
 }

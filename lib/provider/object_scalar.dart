9c642e92e86a8286f3925132df387582d3a5d894
yeasin50 <yeasinsheikh50@gmail.com>
2022-02-26 23:28:24 +0600

    fixing-size

--- lib/provider/object_scalar.dart
+++ lib/provider/object_scalar.dart
@@ -13,6 +13,8 @@ abstract class _Elements {
 
   /// prefer bullet Size(x,4x)
   Size get enemyBullet;
+
+  Size get healthBox;
 }
 
 /// define GameObject's scale, provide information about screen and element size
@@ -31,7 +33,7 @@ class GObjectSize implements _Elements {
 
   Size? _screenSize;
 
-  ///it will decide the object scale, it will be
+  ///it will decide the object scale, min(Screen.height, screen.width)
   late double _minLenght;
 
   /// inital the screenSize, use to generate others elements size
@@ -49,15 +51,31 @@ class GObjectSize implements _Elements {
   @override
   Size get screen => _screenSize ?? _showErr();
 
-  //todo: make responsive
+  //todo: make responsive size
   @override
   Size get enemyShip => const Size(24, 24);
 
+  /// ship size is depend on screen size [_minLenght * .075], min ship  Size(50, 50)
   @override
   Size get playerShip {
     debugPrint("screen size: $screen");
+    _showErr();
+    const double _scale = .075;
+    return Size(
+      _minLenght * _scale,
+      _minLenght * _scale,
+    );
+  }
 
-    return const Size(50, 50);
+  @override
+  Size get healthBox {
+    debugPrint("screen size: $screen");
+    _showErr();
+    const double _scale = .05;
+    return Size(
+      _minLenght * _scale,
+      _minLenght * _scale,
+    );
   }
 
   @override

81a107dbca5fe8950b3929b3765d5cfb4e57153f
yeasin50 <yeasinsheikh50@gmail.com>
2022-03-20 18:28:46 +0600

    added-ship-name

--- lib/model/ship.dart
+++ lib/model/ship.dart
@@ -1,11 +1,31 @@
 import 'package:flutter/material.dart';
 
 import '../provider/provider.dart';
+import '../screens/on_play/utils/enemy_hanlader.dart';
 import 'model.dart';
 
+// defines the ship color/image
+enum ShipName {
+  player,
+  enemyA,
+  enemyB,
+  enemyC,
+}
+
+/// currently we have two image per enemyShip
+enum ShipImageState { a, b }
+
 abstract class IShip implements GameObject {
-  //ship Health per life
-  late final IShipHealth health;
+  ///ship Health per life
+  final IShipHealth health;
+
+  /// ship name will define color/image
+  final ShipName name;
+
+  IShip({
+    required this.health,
+    required this.name,
+  });
 }
 
 class Player implements IShip {
@@ -13,9 +33,7 @@ class Player implements IShip {
 
   Player({
     required Vector2 position,
-  }) {
-    _position = position;
-  }
+  }) : _position = position;
 
   ///max number of time player can live
   final int maxLive = 3;
@@ -34,29 +52,38 @@ class Player implements IShip {
 
   @override
   IShipHealth health = PlayerHealthManager();
+
+  @override
+  ShipName get name => ShipName.player;
 }
 
 class EnemyShip implements IShip {
-  late final Vector2 _position;
-  late final Color _color;
+  final Vector2 _position;
+
+  ///image State, helps to animate currently we have two image per ship
+  /// [0,1]
+  // ShipImageState state;
+
+  final ShipName _name;
 
   EnemyShip({
     required Vector2 position,
-    Color color = Colors.pink,
-  }) {
-    _position = position;
-    _color = color;
-  }
+    ShipName? name,
+  })  : _position = position,
+        _name = name ?? randomEnemyName;
 
   @override
-  Size size =  GObjectSize.instatnce.enemyShip;
+  Size size = GObjectSize.instatnce.enemyShip;
 
   @override
-  Color get color => _color;
+  Color get color => getShipColor(shipName: name);
 
   @override
   Vector2 get position => _position;
 
   @override
   IShipHealth health = NEnemyHealthManager();
+
+  @override
+  ShipName get name => _name;
 }

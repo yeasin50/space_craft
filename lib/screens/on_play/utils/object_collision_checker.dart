f56d4a923661084c3f7dc5900e69709319543aae
yeasin50 <yeasinsheikh50@gmail.com>
2022-04-01 21:09:08 +0600

    added-border-detection

--- lib/screens/on_play/utils/object_collision_checker.dart
+++ lib/screens/on_play/utils/object_collision_checker.dart
@@ -1,4 +1,8 @@
+import 'dart:ui';
+
 import 'package:space_craft/model/model.dart';
+
+import '../../../provider/provider.dart';
 
 /// check collision between [GameObject]
 bool collisionChecker({
@@ -12,4 +16,36 @@ bool collisionChecker({
     return true;
   }
   return false;
+}
+
+//todo: use to show effect, we may just return Offset instead of enmus
+enum BlockedSide { top, left, right, bottom }
+
+///playerShip border collision
+List<BlockedSide> playerMoveable({
+  required Size playerSize,
+  required Vector2 touchPosition,
+}) {
+  final double posY = touchPosition.dY;
+  final double posX = touchPosition.dX;
+
+  List<BlockedSide> blocked = [];
+
+  if (posY >= GObjectSize.instatnce.screen.height - playerSize.height / 2) {
+    blocked.add(BlockedSide.bottom);
+  }
+
+  if (posY <= playerSize.height / 2) {
+    blocked.add(BlockedSide.top);
+  }
+
+  if (posX >= GObjectSize.instatnce.screen.width - playerSize.width / 2) {
+    blocked.add(BlockedSide.right);
+  }
+
+  if (posX <= playerSize.width / 2) {
+    blocked.add(BlockedSide.left);
+  }
+
+  return blocked;
 }

d012b5fd23693cb6ea64950b37f42c76d2f35371
yeasin50 <yeasinsheikh50@gmail.com>
2022-06-13 21:30:59 +0600

    task-to-rm

--- lib/core/widget/circle_neon_border.dart
+++ lib/core/widget/circle_neon_border.dart
@@ -1,125 +1,127 @@
-import 'dart:math';
+// import 'dart:math';
 
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-import '../constants/color_palette.dart';
+// import '../constants/color_palette.dart';
 
-/// A circular neon Background/frame with little [Animation] of [Colors]
-///this is repeated Animation using 4 colors.
-class NeonCircle extends StatefulWidget {
-  ///Duration of animation default=`Duration(seconds: 2)`
-  final Duration duration;
+// // TODO: delete after check
 
-  /// default Curve= `Curves.ease`
-  final Curve curve;
+// /// A circular neon Background/frame with little [Animation] of [Colors]
+// ///this is repeated Animation using 4 colors.
+// class NeonCircle extends StatefulWidget {
+//   ///Duration of animation default=`Duration(seconds: 2)`
+//   final Duration duration;
 
-  /// Main Widget inside Frame
-  final Widget child;
+//   /// default Curve= `Curves.ease`
+//   final Curve curve;
 
-  ///radius of widget
-  final double radius;
+//   /// Main Widget inside Frame
+//   final Widget child;
 
-  ///The Space between child and outer Container/frame known as NeonCircleBackground
-  final double frameThickness;
+//   ///radius of widget
+//   final double radius;
 
-  const NeonCircle(
-      {Key? key,
-      this.duration = const Duration(seconds: 2),
-      this.curve = Curves.ease,
-      required this.radius,
-      this.frameThickness = 9,
-      required this.child})
-      : super(key: key);
+//   ///The Space between child and outer Container/frame known as NeonCircleBackground
+//   final double frameThickness;
 
-  @override
-  _NeonCircleState createState() => _NeonCircleState();
-}
+//   const NeonCircle(
+//       {Key? key,
+//       this.duration = const Duration(seconds: 2),
+//       this.curve = Curves.ease,
+//       required this.radius,
+//       this.frameThickness = 9,
+//       required this.child})
+//       : super(key: key);
 
-class _NeonCircleState extends State<NeonCircle>
-    with SingleTickerProviderStateMixin {
-  late Animation _animation;
-  late AnimationController _controller;
+//   @override
+//   _NeonCircleState createState() => _NeonCircleState();
+// }
 
-  final Random random = Random();
+// class _NeonCircleState extends State<NeonCircle>
+//     with SingleTickerProviderStateMixin {
+//   late Animation _animation;
+//   late AnimationController _controller;
 
-  _initWave() {
-    _controller = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 2),
-    )..addListener(() => setState(() {}));
+//   final Random random = Random();
 
-    _animation = Tween<double>(begin: 0, end: 270).animate(
-      CurvedAnimation(
-        parent: _controller,
-        curve: const Interval(
-          0.0,
-          1.0,
-          curve: Curves.ease,
-        ),
-      ),
-    );
+//   _initWave() {
+//     _controller = AnimationController(
+//       vsync: this,
+//       duration: const Duration(seconds: 2),
+//     )..addListener(() => setState(() {}));
 
-    _controller.repeat(reverse: true);
-  }
+//     _animation = Tween<double>(begin: 0, end: 270).animate(
+//       CurvedAnimation(
+//         parent: _controller,
+//         curve: const Interval(
+//           0.0,
+//           1.0,
+//           curve: Curves.ease,
+//         ),
+//       ),
+//     );
 
-  @override
-  void initState() {
-    super.initState();
-    _initWave();
-  }
+//     _controller.repeat(reverse: true);
+//   }
 
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
+//   @override
+//   void initState() {
+//     super.initState();
+//     _initWave();
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return buildChildCircle();
-  }
+//   @override
+//   void dispose() {
+//     _controller.dispose();
+//     super.dispose();
+//   }
 
-  Stack buildChildCircle() {
-    return Stack(
-      alignment: Alignment.center,
-      children: [
-        Transform.rotate(
-          angle: _animation.value * pi / 180,
-          child: Container(
-            height: widget.radius + 9,
-            width: widget.radius + 9,
-            decoration: BoxDecoration(
-              boxShadow: [
-                BoxShadow(
-                  blurRadius: 6,
-                  color: _animation.value < 55
-                      ? colorSet1[random.nextInt(colorSet1.length - 1)]
-                      : Colors.transparent,
-                  spreadRadius: 2,
-                ),
-              ],
-              shape: BoxShape.circle,
-              gradient: LinearGradient(
-                colors: _animation.value < 50
-                    ? [
-                        colorSet1[0],
-                        colorSet1[3],
-                      ]
-                    : colorSet1,
-              ),
-            ),
-          ),
-        ),
-        ClipOval(
-          child: ConstrainedBox(
-            constraints: BoxConstraints(
-              maxHeight: widget.radius,
-              maxWidth: widget.radius,
-            ),
-            child: widget.child,
-          ),
-        ),
-      ],
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return buildChildCircle();
+//   }
+
+//   Stack buildChildCircle() {
+//     return Stack(
+//       alignment: Alignment.center,
+//       children: [
+//         Transform.rotate(
+//           angle: _animation.value * pi / 180,
+//           child: Container(
+//             height: widget.radius + 9,
+//             width: widget.radius + 9,
+//             decoration: BoxDecoration(
+//               boxShadow: [
+//                 BoxShadow(
+//                   blurRadius: 6,
+//                   color: _animation.value < 55
+//                       ? colorSet1[random.nextInt(colorSet1.length - 1)]
+//                       : Colors.transparent,
+//                   spreadRadius: 2,
+//                 ),
+//               ],
+//               shape: BoxShape.circle,
+//               gradient: LinearGradient(
+//                 colors: _animation.value < 50
+//                     ? [
+//                         colorSet1[0],
+//                         colorSet1[3],
+//                       ]
+//                     : colorSet1,
+//               ),
+//             ),
+//           ),
+//         ),
+//         ClipOval(
+//           child: ConstrainedBox(
+//             constraints: BoxConstraints(
+//               maxHeight: widget.radius,
+//               maxWidth: widget.radius,
+//             ),
+//             child: widget.child,
+//           ),
+//         ),
+//       ],
+//     );
+//   }
+// }
--- lib/core/widget/widget.dart
+++ lib/core/widget/widget.dart
@@ -1,5 +1,4 @@
-export 'circle_neon_border.dart';
+ 
 export 'glassmorphism.dart';
-export '../package/magic_ball/widgets/magic_ball.dart';
 export 'rotate_widget.dart';
 export 'rotational_blast_ring.dart';

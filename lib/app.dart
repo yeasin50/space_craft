0d9aee947264f213e8d5a11a4211e33a173b960a
yeasin50 <yeasinsheikh50@gmail.com>
2022-07-12 22:39:17 +0600

    temp

--- lib/app.dart
+++ lib/app.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:space_craft/feature/on_play/on_play.dart';
 
 import 'core/providers/object_scalar.dart';
 import 'core/routes/app_routes.dart';
 import 'core/themes/app_theme.dart';
 import 'feature/home/home_page.dart';
-
+ 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 

0923eff18598d15214a6f5860c98b29a6871102d
yeasin50 <yeasinsheikh50@gmail.com>
2022-07-28 03:02:00 +0600

    added-ship_paint

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,38 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'app.dart';
+import 'core/utils/paints/paints.dart';
 
 void main() {
   runApp(
     const ProviderScope(
-      child: App(),
+      child: MaterialApp(
+        home: _testPaint(),
+      ),
     ),
   );
+}
+
+class _testPaint extends StatelessWidget {
+  const _testPaint({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final double size = 240;
+
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Container(
+          color: Colors.cyanAccent.withOpacity(.3),
+          width: size,
+          height: size,
+          child: CustomPaint(
+            size: Size(size, size),
+            painter: EnemyAPainter(),
+          ),
+        ),
+      ),
+    );
+  }
 }

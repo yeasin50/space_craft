e6c4ebbd7dae04c6fc05c30db75b84de14d762f8
yeasin50 <yeasinsheikh50@gmail.com>
2022-08-14 03:09:42 +0600

    fixed-MatrixB

--- lib/feature/on_play/models/invader_matrix.dart
+++ lib/feature/on_play/models/invader_matrix.dart
@@ -54,18 +54,23 @@ class InvaderMatrixA extends Matrix8x12 implements _InvaderMatrixState {
 class InvaderMatrixB extends Matrix8x12 implements _InvaderMatrixState {
   /// Invader B ship on X state
   static Matrix8x12 get _x {
-    final model = Matrix8x12.head()
-      ..clearRowAndSetCells(
-          rowIndex: 0, cellIndexes: [3, 4, 5, 6, 7, 8], value: 1)
-      ..setEntry(row: 3, col: 4, v: 1)
-      ..setEntry(row: 3, col: 7, v: 1)
-      ..clearRowAndSetCells(
-          rowIndex: 5, cellIndexes: [1, 2, 4, 5, 6, 7, 9, 10], value: 1)
-      ..clearRowAndSetCells(rowIndex: 6, cellIndexes: [1, 2, 10, 11], value: 1)
-      ..clearRowAndSetCells(rowIndex: 6, cellIndexes: [0, 1, 10, 11], value: 1)
-      ..clearRowAndSetCells(rowIndex: 7, cellIndexes: [2, 3, 8, 9], value: 1);
-
-    return model;
+    return Matrix8x12.head()
+          ..clearRowAndSetCells(rowIndex: 0, cellIndexes: [2, 9], value: 1)
+          ..clearRowAndSetCells(rowIndex: 1, cellIndexes: [3, 8], value: 1)
+          ..fillRow(rowIndex: 2, value: 0)
+          ..fillRow(rowIndex: 2, startCellIndex: 2, endCellIndex: 9)
+          ..clearRowAndSetCells(
+              rowIndex: 3, cellIndexes: [1, 2, 4, 5, 6, 7, 9, 10], value: 1)
+          ..fillRow(rowIndex: 4)
+          ..fillRow(rowIndex: 5)
+          ..setEntry(row: 5, col: 1, v: 0)
+          ..setEntry(row: 5, col: 10, v: 0)
+          ..clearRowAndSetCells(
+              rowIndex: 6, cellIndexes: [0, 2, 9, 11], value: 1)
+          ..clearRowAndSetCells(
+              rowIndex: 7, cellIndexes: [3, 4, 7, 8], value: 1)
+        //
+        ;
   }
 
   /// Invader B ship on Y state
@@ -73,6 +78,10 @@ class InvaderMatrixB extends Matrix8x12 implements _InvaderMatrixState {
     return _x
       ..clearRowAndSetCells(rowIndex: 0, cellIndexes: [2, 9], value: 1)
       ..clearRowAndSetCells(rowIndex: 1, cellIndexes: [0, 3, 8, 11], value: 1)
+      ..setEntry(row: 2, col: 0, v: 1)
+      ..setEntry(row: 3, col: 0, v: 1)
+      ..setEntry(row: 2, col: 11, v: 1)
+      ..setEntry(row: 3, col: 11, v: 1)
       ..fillRow(rowIndex: 5, value: 0)
       ..fillRow(rowIndex: 5, startCellIndex: 1, endCellIndex: 10, value: 1)
       ..clearRowAndSetCells(rowIndex: 6, cellIndexes: [2, 9], value: 1)
@@ -88,7 +97,7 @@ class InvaderMatrixB extends Matrix8x12 implements _InvaderMatrixState {
 
 ///Enemy ship C with two State
 class InvaderMatrixC extends Matrix8x12 implements _InvaderMatrixState {
-  /// Invader B ship on X state
+  /// Invader B sh ip on X state
   static Matrix8x12 get _x {
     final model = Matrix8x12.head()
       ..clearRowAndSetCells(

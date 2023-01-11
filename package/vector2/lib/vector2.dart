b60f56d92ef884017ca3295be8c9d48a04462502
yeasin50 <yeasinsheikh50@gmail.com>
2023-01-11 06:13:32 +0600

    init

---
+++ package/vector2/lib/vector2.dart
@@ -0,0 +1,11 @@
+library vector2;
+
+import 'package:equatable/equatable.dart';
+import 'package:flutter/painting.dart';
+
+part 'src/vector2.dart';
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}

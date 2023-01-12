a1c94a98708d7835ae7688b24dd6396ea3200aa6
yeasin50 <yeasinsheikh50@gmail.com>
2023-01-12 21:22:56 +0600

    added-src

--- package/magic_ball/lib/magic_ball.dart
+++ package/magic_ball/lib/magic_ball.dart
@@ -1,3 +1,9 @@
 library magic_ball;
 
- 
+import 'dart:async';
+import 'dart:math' as math;
+
+import 'package:flutter/material.dart';
+
+part 'src/magic_ball_widget.dart';
+part 'src/particle_path.dart';

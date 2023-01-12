919b65a026124fe51edc8f70d62edf610a577dbb
yeasin50 <yeasinsheikh50@gmail.com>
2023-01-12 22:11:52 +0600

    init-pckg

---
+++ package/glitch_effect/lib/src/utils/glitch_clipper.dart
@@ -0,0 +1,36 @@
+part of glitch_effect;
+
+class GlitchClipper extends CustomClipper<Path> {
+  final deltaMax = 15;
+  final min = 3;
+  final random = math.Random();
+  @override
+  getClip(Size size) {
+    Path path = Path();
+    double y = randomStep;
+    while (y < size.height) {
+      double yRandom = randomStep;
+      double x = randomStep;
+
+      while (x < size.width) {
+        double xRandom = randomStep;
+        path.addRect(
+          Rect.fromPoints(
+            Offset(x, y.toDouble()),
+            Offset(x + xRandom, y + yRandom),
+          ),
+        );
+        x += randomStep * 2;
+      }
+      y += yRandom;
+    }
+
+    path.close();
+    return path;
+  }
+
+  double get randomStep => min + random.nextInt(deltaMax).toDouble();
+
+  @override
+  bool shouldReclip(covariant GlitchClipper oldClipper) => this != oldClipper;
+}
